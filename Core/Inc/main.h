42adc5de884f07b6fa5ee2c1abfc87b733d55a17
Oriol1750391 <1750391@campus.euss.org>
2026-08-12 23:29:26 +0200

    Configuració del .ioc (CAN+SupplyLED+Clock) i totes les variables que es rebrán per CAN

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,9 +57,100 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DfSUPled_Pin GPIO_PIN_3
+#define DfSUPled_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
+typedef struct {
+	//Front ECU
+	uint8_t FpDIGRpot;
+	uint8_t FpDIGLpot;
+	uint8_t FpDIGRvel;
+	uint8_t FpDIGLvel;
+	uint8_t FpANLbrake;
+	uint8_t FpINTtsoff;
+	uint8_t FpINTsbms;
+	uint8_t FpINTr2d;
+	uint8_t FpINTmenu;
+	uint8_t FpDIGmicrosd;
+	uint8_t FpSDCinertia;
+	uint8_t FpSDCbots;
+	uint8_t FpSDCcsdb;
+	uint8_t FpERRapps;
+	uint8_t FpDIGrefri;
+	uint8_t FpDIGr2d;
+	uint8_t FpINTrefrion;
+	uint8_t FpDIGvel;
+	uint16_t FpSHU;
+	uint16_t FpANLRpot;
 
+	//Rear ECU
+	uint16_t RpSIGlvs;
+	uint16_t RpSHU;
+	uint8_t RpSDChvd;
+	uint8_t RpSDCtsms;
+	uint8_t RpSDClsdb;
+	uint8_t RpSDCrsdb;
+	uint8_t RpSTAbrkledR;
+	uint8_t RpSTAbrkledG;
+	uint8_t RpSTAbrkledB;
+	uint8_t FpANLbrake;
+	uint8_t FpDIGr2d;
+	uint8_t FpINTrefrion;
+	uint8_t IpRPM;
+	uint8_t IpI;
+	uint8_t IpPar;
+	uint8_t IpV;
+	uint8_t IpT_IGBT;
+	uint8_t IpT_Mot;
+	uint8_t IpErrL1;
+	uint8_t IpErrH1;
+	uint8_t IpErrL2;
+	uint8_t IpErrH2;
+
+	//TSALG
+	uint8_t TpDIGspre;
+	uint8_t TpDIGsairp;
+	uint8_t TpDIGsairn;
+	uint8_t TpDIGipre;
+	uint8_t TpDIGiairp;
+	uint8_t TpDIGiairn;
+	uint8_t TpTHRhv;
+	uint8_t TpERRscs;
+	uint8_t TpTHRdis;
+	uint8_t TpLCH;
+	uint8_t TpINTled;
+
+	//HVAB
+	uint8_t ApTHRhv;
+	uint16_t ApSHU;
+
+	//HVDB
+	uint8_t BpTHRbrake;
+	uint8_t BpTHRcurrent;
+	uint8_t BpERRplaus;
+	uint8_t DpINTtsalred;
+	uint8_t BpSDC;
+	uint8_t DpSDC;
+	uint8_t DpTHRhv;
+	uint8_t DpLCHdischarge;
+	uint8_t DpSDCintlck1;
+	uint8_t DpSDCintlck2;
+	uint16_t BpSHU;
+	uint16_t DpSHU;
+
+	//SDCReset
+	uint8_t SpERRbms;
+	uint8_t SpERRimd;
+	uint8_t SpLCHebms;
+	uint8_t SpLCHeimd;
+	uint8_t SpINTresbut;
+	uint8_t SpSDCbms;
+	uint8_t SpSDCimd;
+	uint16_t SpSHU;
+} DICCP_t;
+
+extern volatile DICCP_t DICCP;
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,12 +41,16 @@
 
 /* Private variables ---------------------------------------------------------*/
 
+FDCAN_HandleTypeDef hfdcan1;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_FDCAN1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -84,6 +88,8 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_FDCAN1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -135,6 +141,81 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief FDCAN1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_FDCAN1_Init(void)
+{
+
+  /* USER CODE BEGIN FDCAN1_Init 0 */
+
+  /* USER CODE END FDCAN1_Init 0 */
+
+  /* USER CODE BEGIN FDCAN1_Init 1 */
+
+  /* USER CODE END FDCAN1_Init 1 */
+  hfdcan1.Instance = FDCAN1;
+  hfdcan1.Init.ClockDivider = FDCAN_CLOCK_DIV1;
+  hfdcan1.Init.FrameFormat = FDCAN_FRAME_CLASSIC;
+  hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;
+  hfdcan1.Init.AutoRetransmission = ENABLE;
+  hfdcan1.Init.TransmitPause = DISABLE;
+  hfdcan1.Init.ProtocolException = DISABLE;
+  hfdcan1.Init.NominalPrescaler = 12;
+  hfdcan1.Init.NominalSyncJumpWidth = 1;
+  hfdcan1.Init.NominalTimeSeg1 = 13;
+  hfdcan1.Init.NominalTimeSeg2 = 2;
+  hfdcan1.Init.DataPrescaler = 1;
+  hfdcan1.Init.DataSyncJumpWidth = 1;
+  hfdcan1.Init.DataTimeSeg1 = 1;
+  hfdcan1.Init.DataTimeSeg2 = 1;
+  hfdcan1.Init.StdFiltersNbr = 0;
+  hfdcan1.Init.ExtFiltersNbr = 0;
+  hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
+  if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN FDCAN1_Init 2 */
+
+  /* USER CODE END FDCAN1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(DfSUPled_GPIO_Port, DfSUPled_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : DfSUPled_Pin */
+  GPIO_InitStruct.Pin = DfSUPled_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DfSUPled_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
